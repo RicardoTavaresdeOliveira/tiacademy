76afaca3876a2c8669da45420f62622e2e6f9ea5
Ricardo Tavares <88669418+RicardoTavaresdeOliveira@users.noreply.github.com>
2021-08-25 17:41:36 -0300

    Add files via upload

---
+++ ciclo 2/aula 4/atividade de reprodução/kitanda/produtos.js
@@ -0,0 +1,52 @@
+
+
+var produtos = [
+
+	{prodDesc:"Mamão Papaia", prodPreco : 4.70},
+	{prodDesc:"Mamão Papaia", prodPreco : 3.70},
+	{prodDesc:"Manga", prodPreco : 2.80},
+	{prodDesc:"Melão", prodPreco : 6.90},
+	{prodDesc:"Melância", prodPreco : 5.00},
+	
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
