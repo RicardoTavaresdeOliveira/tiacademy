4bd09bdcaff1513ddea5cbb420da3a8d8d5c2feb
Ricardo Tavares <88669418+RicardoTavaresdeOliveira@users.noreply.github.com>
2021-08-25 15:10:18 -0300

    Add files via upload

---
+++ ciclo 2/aula 1/atividade de reprodução/script.js
@@ -0,0 +1,64 @@
+/* comentario no JS*/
+
+// comentario em uma linha
+
+
+/*
+var nome = "Ricardo"; 
+
+console.log(typeof (nome));
+
+var nome = 12;
+
+console.log(typeof(nome));
+
+document.write("Idade" + nome + "<br>");
+*/
+
+
+var fruta = "Laranja"; 
+var oque = "doce";  
+var tudoJunto = fruta+" "+oque; //terceira variavel buscou as outras duas
+
+//console.log(tudoJunto);  
+
+var valor1 = '2';
+var valor2 = 2; 
+var total = valor1 + valor2;
+
+
+document.write(total); //saindo no html, na página do navegador
+console.log(valor1 - valor2); //console do navegador
+
+var i = 2;
+var j = 3; 
+
+console.log(i%j);  
+
+var  x = 3;
+var y = 2; 
+
+console.log(x * y);
+
+var v = (valor1 == valor2);  
+console.log(v)
+
+var v = (valor1 === valor2); 
+console.log(v)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ciclo 2/aula 1/atividade de reprodução/script2.js
@@ -0,0 +1,24 @@
+
+/*
+	alert
+	prompt
+	confirm
+*/
+
+
+var aula = "Javascript";
+
+alert("Aula de" + aula); //enviar uma caixa de alerta
+
+var teste = prompt("Digite uma fruta"); // caixa de entrada
+
+document.write("Uma fruta" + teste); // imprime no html
+
+var resposta = confirm("Clique um dos botões");  
+
+console.log(resposta);  
+
+
+
+
+
